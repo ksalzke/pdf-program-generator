7485bd4c5907eb0060242b656a28db708748a07e
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-07-18 11:44:14 +0200

    Initial commit
    Hello World prints in PDF which is automatically downloaded
    but does not display in browser

---
+++ script.js
@@ -0,0 +1,26 @@
+var doc = new PDFDocument();
+var stream = doc.pipe(blobStream());
+
+doc.text("Hello World");
+
+// end and display the document in the iframe to the right
+doc.end();
+
+var saveData = (function() {
+  var a = document.createElement("a");
+  console.log(document);
+  $("body").append(a);
+  a.style = "display: none";
+  return function(blob, fileName) {
+    var url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  };
+})();
+
+stream.on("finish", function() {
+  var blob = stream.toBlob("application/pdf");
+  saveData(blob, "aa.pdf");
+});

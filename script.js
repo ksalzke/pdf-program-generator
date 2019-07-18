fe35c55263d682db619017aa3b097b695c63aac5
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-07-18 19:45:56 +0200

    Vertically align row centre

--- script.js
+++ script.js
@@ -8,30 +8,72 @@ var stream = doc.pipe(blobStream());
 
 let columnWidth = (doc.page.width - margin * 2) / 3;
 
+function getHeightOfString(item) {
+  return doc.heightOfString(item[0], item[1]);
+}
+
+function getMaximumHeight(item1, item2, item3) {
+  let item1Height = getHeightOfString(item1);
+  let item2Height = getHeightOfString(item2);
+  let item3Height = getHeightOfString(item3);
+  return Math.max(item1Height, item2Height, item3Height);
+}
+
+function getYOffset(currentItemHeight, maximumHeight) {
+  if (currentItemHeight == maximumHeight) {
+    return 0;
+  } else {
+    return (maximumHeight - currentItemHeight) / 2;
+  }
+}
+
 for (let i = 0; i < concertData.performances.length; i++) {
   let performance = concertData.performances[i];
 
-  var currentYCoord = doc.y;
+  let currentYCoord = doc.y;
 
+  let studentText = [
+    performance.student,
+    {
+      width: columnWidth
+    }
+  ];
+  let pieceText = [
+    performance.piece,
+    {
+      align: "center",
+      width: columnWidth
+    }
+  ];
+  let composerText = [
+    performance.composer,
+    {
+      align: "right",
+      width: columnWidth
+    }
+  ];
+
+  let maximumHeight = getMaximumHeight(studentText, pieceText, composerText);
+
+  let studentOffset = getYOffset(getHeightOfString(studentText), maximumHeight);
+  doc.y = doc.y + studentOffset;
   doc.x = margin;
-  doc.fontSize(11).text(performance.student, {
-    width: columnWidth
-  });
+  doc.fontSize(11).text(studentText[0], studentText[1]);
   let yAfterStudent = doc.y;
-  doc.x = margin + columnWidth;
-  doc.y = currentYCoord;
-  doc.text(performance.piece, {
-    align: "center",
-    width: columnWidth
-  });
-  let yAfterPiece = doc.y;
-  doc.x = margin + columnWidth * 2;
-  doc.y = currentYCoord;
-  doc.text(performance.composer, {
-    align: "right",
-    width: columnWidth
-  });
 
+  let pieceOffset = getYOffset(getHeightOfString(pieceText), maximumHeight);
+  doc.x = margin + columnWidth;
+  doc.y = currentYCoord + pieceOffset;
+  doc.text(pieceText[0], pieceText[1]);
+  let yAfterPiece = doc.y;
+
+  let composerOffset = getYOffset(
+    getHeightOfString(composerText),
+    maximumHeight
+  );
+  doc.x = margin + columnWidth * 2;
+  doc.y = currentYCoord + composerOffset;
+  doc.text(composerText[0], composerText[1]);
   let yAfterComposer = doc.y;
 
   doc.x = margin;
